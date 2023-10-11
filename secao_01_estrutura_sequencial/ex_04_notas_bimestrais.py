3c86bce5da8ce1174374e7dbf30f6d3c60102d87
Gabriel Galdino <gabrielgaldinodev@gmail.com>
2023-10-10 21:28:01 -0300

    resoluçao notas bimestrais

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -15,4 +15,13 @@ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
 def calcular_media():
     """Escreva aqui em baixo a sua solução"""
+    nota1 = int(input('Digite sua nota'))
+    nota2 = int(input('Digite sua nota'))
+    nota3 = int(input('Digite sua nota'))
+    nota4 = int(input('Digite sua nota'))
+
+
+    resultado = (nota1 + nota2 + nota3 + nota4) / 4
+
+    print(f'A média anual é {resultado}')
 
